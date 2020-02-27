906b62e1c41ed24f918f5ff720b59f4ecd17308c
Trevor Henderson <trevorsgit@gmail.com>
2020-02-27 17:25:40 -0600

    Initialize express app. Setup ES 6 support. create script to run server (yarn start)

---
+++ README.md
@@ -0,0 +1,3 @@
+# User Auth Service
+A service written in express for the purpose of authentication users for the _Next Episode_
+React App
---
+++ app.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+app.use(cors());
+
+app.get('/', (rew,res) => {
+   res.send('Hello Express');
+});
+
+app.listen(process.env.PORT, () =>
+    console.log('ESM 6 running on node!'),
+);
